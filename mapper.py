d10465e75080aa383aeaa9fd66e4c41166ff778b
swathichandana <55080414+swathichandana@users.noreply.github.com>
2019-09-09 23:27:44 +0530

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+# The Mapper
+import sys
+
+# Set local variables
+currentCountry = None
+previousCountry = None
+currentFx = None
+previousFx = None
+percentChange = None
+currentKey = None
+fxMap = []
+
+infile = sys.stdin
+
+next(infile) # skip first line of input file
+
+for line in infile:
+    line = line.strip()
+    line = line.split(',', 2)
+
+    try:
+        # Get data from line
+        currentCountry = line[1].rstrip()
+        if len(line[2]) == 0:
+            continue
+        currentFx = float(line[2])
+
+        if currentCountry != previousCountry:
+            previousCountry = currentCountry
+            previousFx = currentFx
+            previousLine = line
+            continue
+
+        # If country same as previous, add to map
+        elif currentCountry == previousCountry:
+            percentChange = ((currentFx - previousFx) / previousFx) * 100.00
+            percentChange = round(percentChange, 2)
+            percentChange = percentChange
+
+        currentKey = (currentCountry, percentChange)
+
+        # Set the array with tuple keys
+        fxMap.append(currentKey)
+
+        # Update Values
+        previousCountry = currentCountry
+        previousFx = currentFx
+        previousLine = line
+
+    # Handle unexpected errors
+    except Exception as e:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(e).__name__, e.args)
+        print("currentFx: %.2f previousFx: %.2f" % (currentFx, previousFx))
+        print(message)
+        sys.exit(1)
+
+# Show the returned values
+for i in sorted(fxMap):
+    print(",".join([str(x) for x in i]))
---
+++ reducer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+from collections import defaultdict
+counts = defaultdict(int)
+for line in sys.stdin:
+    (country, percent_change) = line.strip().split(",")
+    counts[(country, percent_change)] += 1
+for key in sorted(counts):
+    print("{} @ {}% = {}".format(key[0], key[1], counts[key]))
